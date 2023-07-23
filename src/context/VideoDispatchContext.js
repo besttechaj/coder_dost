80f0e167445364478ec28f0073109088ece13bca
Ajay Mishra <asap2656@gmail.com>
2023-07-23 14:04:42 +0530

    here ends the combining of contextApi and useReducer Hooks

--- src/context/VideoDispatchContext.js
+++ src/context/VideoDispatchContext.js
@@ -1,4 +1,6 @@
 import { createContext } from 'react';
+
+//initializing the value of createContext with by default value that is null but we are passing the original value inside provider
 const VideoDispatchContext = createContext(null);
 
 export default VideoDispatchContext;
--- src/context/VideosDataContext.js
+++ src/context/VideosDataContext.js
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
+//initializing the value of createContext with by default value that is null but we are passing the original value inside provider
 const VideosDataContext = createContext(null);
 
 export default VideosDataContext;
